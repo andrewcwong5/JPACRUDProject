e4c4f7e0296c5acbd0b435e1626a040359a1cd2a
Andrew Wong <ndrwchkwng@yahoo.com>
2019-11-08 15:48:56 -0700

    Stub JPA/Boot objects, connect projects, stub and test Buffet entity

---
+++ BuffetLocations/src/main/java/com/skilldistillery/buffets/BuffetLocationsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.buffets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuffetLocationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuffetLocationsApplication.class, args);
+	}
+
+}
